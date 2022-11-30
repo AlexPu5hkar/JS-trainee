dcd0dddfd33b340a29a6a8e1fc61c71c9ae2d3fd
Alex <pushkaralexander1@gmail.com>
2022-11-30 19:05:56 +0400

    second

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-console.log(13)
+"useStrict";
+
+let number = 5.2;
+
+console.log(-4/0);
+console.log('string'*9);
+
+const person = "Alex";
+const age = '37';
+
+const bool = false;
+
+/*console.lof(something);*/
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+/*console.log(obj.isMarried);*/
+console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpeg', 5, 'strawberry.gif', {}, [] ];
+console.log(arr[1]);
+
