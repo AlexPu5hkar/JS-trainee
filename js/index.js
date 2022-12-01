37871e26d14129b216f5adffa217517f89aa9629
Alex <pushkaralexander1@gmail.com>
2022-12-01 21:47:05 +0400

    third

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 "useStrict";
 
-let number = 5.2;
+/*const answer = +prompt("are you older than 18?", "18");
+console.log(answer + 5);*/
+
+/*const result = confirm("are you here?");
+console.log(result);*/
+
+/*alert("hello");*/
+/*let number = 5.2;
 
 console.log(-4/0);
 console.log('string'*9);
@@ -12,18 +19,25 @@ const bool = false;
 
 /*console.lof(something);*/
 
-let und;
-console.log(und);
+/*let und;
+console.log(und);*/
 
-const obj = {
+/*const obj = {
     name: "John",
     age: 25,
     isMarried: false
-};
+};*/
 
 /*console.log(obj.isMarried);*/
-console.log(obj["name"]);
+/*console.log(obj["name"]);*/
 
-let arr = ['plum.png', 'orange.jpeg', 5, 'strawberry.gif', {}, [] ];
-console.log(arr[1]);
+/*let arr = ['plum.png', 'orange.jpeg', 5, 'strawberry.gif', {}, [] ];
+console.log(arr[1]);*/
 
+const answers = [];
+
+/*answers[0] = prompt('first name?', '');
+answers[1] = prompt('last name?', '');
+answers[2] = prompt('age?', '');*/
+
+console.log(typeof(answers));
