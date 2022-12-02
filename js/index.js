3f0deb5b8c33363ca57d5bcfd5b3e5f726a80999
Alex <pushkaralexander1@gmail.com>
2022-12-02 12:21:45 +0400

    fifth?

--- js/index.js
+++ js/index.js
@@ -34,10 +34,39 @@ console.log(und);*/
 /*let arr = ['plum.png', 'orange.jpeg', 5, 'strawberry.gif', {}, [] ];
 console.log(arr[1]);*/
 
-const answers = [];
+/*const answers = [];*/
 
 /*answers[0] = prompt('first name?', '');
 answers[1] = prompt('last name?', '');
 answers[2] = prompt('age?', '');*/
 
-console.log(typeof(answers));
+/*console.log(typeof(answers));*/
+
+/*const category = "toys";
+
+console.log(`https://anyurl.com/${category}/5`);
+
+const user = "Alex";
+
+alert(`Hello, ${user}`);*/
+
+/*console.log('arr' + "- object");
+console.log(965 + + "654");*/
+
+/*let incr = 10, 
+    decr = 10;*/
+
+/*incr++;
+decr--;*/
+
+/*console.log(++incr);
+console.log(--decr);
+
+console.log(2*4 === 8);
+console.log(incr == decr);
+*/
+
+/*const isChecked = true,
+      isClose = true;
+
+console.log(isChecked || !isClose);*/
