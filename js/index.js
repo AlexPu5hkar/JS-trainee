94a6b6ea788a6b817944b36ee7e5e195b12b9215
Alex <super14hero14@gmail.com>
2022-12-02 19:05:51 +0400

    made questions

--- js/index.js
+++ js/index.js
@@ -1,76 +1,19 @@
-"useStrict";
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", '');
 
-/*const answer = +prompt("are you older than 18?", "18");
-console.log(answer + 5);*/
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-/*const result = confirm("are you here?");
-console.log(result);*/
+const a = prompt('Один из последних просмотренных фильмов?' , ''),
+      b = prompt('На сколько его оцените?', ''),
+      c = prompt('Один из последних просмотренных фильмов?' , ''),
+      d = prompt('На сколько его оцените?', '');
 
-/*alert("hello");*/
-/*let number = 5.2;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
 
-console.log(-4/0);
-console.log('string'*9);
-
-const person = "Alex";
-const age = '37';
-
-const bool = false;
-
-/*console.lof(something);*/
-
-/*let und;
-console.log(und);*/
-
-/*const obj = {
-    name: "John",
-    age: 25,
-    isMarried: false
-};*/
-
-/*console.log(obj.isMarried);*/
-/*console.log(obj["name"]);*/
-
-/*let arr = ['plum.png', 'orange.jpeg', 5, 'strawberry.gif', {}, [] ];
-console.log(arr[1]);*/
-
-/*const answers = [];*/
-
-/*answers[0] = prompt('first name?', '');
-answers[1] = prompt('last name?', '');
-answers[2] = prompt('age?', '');*/
-
-/*console.log(typeof(answers));*/
-
-/*const category = "toys";
-
-console.log(`https://anyurl.com/${category}/5`);
-
-const user = "Alex";
-
-alert(`Hello, ${user}`);*/
-
-/*console.log('arr' + "- object");
-console.log(965 + + "654");*/
-
-/*let incr = 10, 
-    decr = 10;*/
-
-/*incr++;
-decr--;*/
-
-/*console.log(++incr);
-console.log(--decr);
-
-console.log(2*4 === 8);
-console.log(incr == decr);
-*/
-
-/*const isChecked = true,
-      isClose = true;
-
-console.log(isChecked || !isClose);*/
- const user = "Alex",
-        age = "37";
-
-console.log(age, user);
+console.log(personalMovieDB);
